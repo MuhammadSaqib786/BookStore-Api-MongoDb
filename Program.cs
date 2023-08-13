bc4c6953637ca4861714d076bb1f03baf74d8663
MuhammadSaqib786 <saqisaqi.sk@gmail.com>
2023-08-13 13:53:47 +0500

    added cors (cross origin to use react)

--- Program.cs
+++ Program.cs
@@ -14,6 +14,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")  // adjust this if your React app is on a different port
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +36,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
+app.UseCors();
 
 app.MapControllers();
 
