3695e6eec48229fee4b9af2f8e36c3a49e3d6f10
MuhammadSaqib786 <saqisaqi.sk@gmail.com>
2023-08-13 12:33:58 +0500

    BooksService class is registered with D

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using BookStore_Api_MongoDb.Models;
+using BookStore_Api_MongoDb.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<BookStoreDatabaseSettings>(
     builder.Configuration.GetSection("BookStoreDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
